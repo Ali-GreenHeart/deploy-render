5af9bfd1fedf7194c2c6e259def4302893168af0
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-03 20:08:34 +0400

    add creating new pet functionality

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 import express from 'express'
-
+import { v4 as generateId } from 'uuid'
+// universally unique id
 const pets = [
     {
-        id: 0,
+        id: "9fb2b4df-bddc-4248-b9f6-15fb7d9b93ab",
         name: 'luna',
         type: 'cat',
         age: 3,
         color: 'brown'
     },
     {
-        id: 1,
+        id: "c6095f0d-30a7-4b6d-bef4-7fda1daaebff",
         name: 'jijolu',
         type: 'dog',
         age: 1,
         color: 'black'
     },
     {
-        id: 2,
+        id: "f5f327c3-016c-409c-bf14-9622ae4b00b5",
         name: 'polika',
         type: 'parrot',
         age: 5,
         color: 'green'
     },
 ]
-
 const app = express()
+app.use(express.json())
 
 app.get("/pets", (req, res) => {
     return res.json(pets)
 })
 app.get("/pets/:id", (req, res) => {
-    return res.json(pets.find(({ id }) => id === +req.params.id))
+    return res.json(pets.find(({ id }) => id === req.params.id))
+})
+
+app.post("/pets", (req, res) => {
+    const id = generateId()
+    console.log(req.body)
+    pets.push({ ...req.body, id })
+    return res.json("Pet successfully added!")
 })
 
 
